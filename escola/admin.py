0e74fd914b7003051f9dc8330f19ba65050f0936
samuelpabl0 <samuelpablooliveiradasilva@gmail.com>
2022-01-06 16:18:23 -0300

    'main'

--- escola/admin.py
+++ escola/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from escola.models import Aluno
 
 class Alunos (admin.ModelAdmin):
-    list_display = ('id','nome','rg','cpf',)
+    list_display = ('id','nome','rg',)
     list_display_links = ('id','nome')
     search_fields = ('nome',)
 
