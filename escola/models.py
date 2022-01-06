fad35f5571c30a0f48377c39b247858389b85a36
samuelpabl0 <samuelpablooliveiradasilva@gmail.com>
2022-01-06 16:14:36 -0300

    'main'

--- escola/models.py
+++ escola/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Aluno (models.Model):
     nome = models.CharField (max_length=30,null=True)
     rg = models.CharField (max_length=9,null=True)
-    cpf =models.CharField(max_length=30,verbose_name='cpf',null=true)
+    cpf =models.CharField(db_column='cpfCnpj', max_length=30, verbose_name='CPF')
 
     def __str__(self):
         return self.nome
